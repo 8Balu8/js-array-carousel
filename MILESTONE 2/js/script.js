3efbb1e0f7db2c8fa1863844bb65131eef0c2084
8Balu8 <barzagliluca@gmail.com>
2021-11-18 18:17:32 +0100

    scaffolding Milstone 2

---
+++ MILESTONE 2/js/script.js
@@ -0,0 +1,12 @@
+// MILESTONE 1
+// Per prima cosa, creiamo il markup statico: costruiamo il container e 
+// inseriamo l'immagine grande a sinistra 
+// e le thumbnails sulla destra in modo da poter stilare lo slider; 
+// avremo così la struttura base e gli stili pronti per poterci poi concentrare solamente sull'aspetto logico.
+
+// MILESTONE 2
+// Adesso rimuoviamo tutto il markup statico e inseriamo le immagini e 
+// i testi delle immagini dinamicamente servendoci dell'array fornito e 
+// un semplice ciclo for che concatena un template literal. 
+// Al termine di questa fase ci ritroveremo con lo stesso slider, 
+// ma costruito dinamicamente attraverso JavaScript.
